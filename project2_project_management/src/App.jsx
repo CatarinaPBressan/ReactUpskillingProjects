5a1cdc5d12d451d827fc9fca92db2c0e0cc9ed85
Catarina Bressan <catarina.bressan@thoughtworks.com>
2026-09-04 14:31:47 -0300

    Finalize CSS

--- project2_project_management/src/App.jsx
+++ project2_project_management/src/App.jsx
@@ -1,83 +1,69 @@
 function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "flex-start",
-        alignItems: "stretch",
-        border: "1px solid green",
-        height: "98vh",
-      }}
-    >
-      <aside
-        style={{
-          border: "1px solid red",
-          height: "100%",
-          flex: "20%",
-          padding: "1rem",
-        }}
-      >
+    <div id="pageContainer">
+      <aside>
         <h1>Your Projects</h1>
         <button>+ Add Project</button>
         <ul>
-          <li>Project 1</li>
+          <li>
+            <button>Project 1</button>
+          </li>
         </ul>
       </aside>
-      <main
-        style={{
-          border: "1px solid blue",
-          flex: "80%",
-          padding: "1rem",
-          overflowY: "auto",
-          height: "100%",
-        }}
-      >
-        <div id="noProjectPage">
-          <h1>No Project Selected</h1>
-          <h2>Select or create a project</h2>
-          <button>Create a Project</button>
-        </div>
-        <div id="createProjectPage">
-          <h1>New Project</h1>
-          <div>
-            <button>Cancel</button>
-            <button>Save</button>
+      <main>
+        <div id="noProjectPage" className="page">
+          <div className="center">
+            <h1>No Project Selected</h1>
+            <h2>Select or create a project</h2>
+            <button>Create a Project</button>
           </div>
+        </div>
+        <div id="createProjectPage" className="page">
+          <h1>New Project</h1>
+
           <section className="inputs">
-            <div className="input-Group">
+            <div className="controls">
+              <button>Cancel</button>
+              <button>Save</button>
+            </div>
+            <div className="input-group">
               <label htmlFor="title">Project title</label>
               <input type="text" id="title" />
             </div>
-            <div className="input-Group">
+            <div className="input-group">
               <label htmlFor="description">Project description</label>
               <textarea id="description" />
             </div>
-            <div className="input-Group">
+            <div className="input-group">
               <label htmlFor="dueDate">Due date</label>
               <input type="date" id="dueDate" />
             </div>
           </section>
         </div>
-        <div id="projectPage">
+        <div id="projectPage" className="page">
           <h1>Project Title</h1>
-          <div>
+          <div className="controls">
             <button>Delete</button>
           </div>
-          <div>04/09/2026</div>
-          <div>
-            <p>Project description</p>
+          <div className="project-info">
+            <p className="due-date">04/09/2026</p>
+            <p className="project-description">Project description</p>
           </div>
-          <div>
+
+          <div className="tasks-container">
             <h2>Tasks</h2>
-            <div>
+            <div className="task-input-group">
               <input type="text" placeholder="Task description" />
               <button>Add task</button>
             </div>
             <div>
-              <p>No tasks added</p>
-              <div>
-                <p>Task 1</p> <button>Clear</button>
-              </div>
+              <ul>
+                <li>No tasks added</li>
+                <li className="task">
+                  <p>Implement the React Project Management project</p>
+                  <button>Clear</button>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
